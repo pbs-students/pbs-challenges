7546416bd70cd0cad8d427c5251bca81f147bf90
Allison Sheridan <allison@podfeet.com>
2023-03-25 20:56:07 -0700

    Feat: add solution, challenge solution, and external menu file

---
+++ bart/pbs-147-bash-arrays/pbs147-challengeSolution-bonusCredit.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# read the menu
+declare -a menu
+while read -r menuLine
+do
+    # skip comment lines
+    echo "$menuLine" | egrep -q '^[ ]*#' && continue
+
+    # skip empty lines
+    echo "$menuLine" | egrep -q '^[ ]*$' && continue
+
+    # store the menu item
+    menu+=("$menuLine")
+done <<<"$(cat $(dirname "$BASH_SOURCE")/menu.txt)"
+
+# create an empty array to hold the order
+declare -a order
+
+# present the menu, with a done option
+echo 'Choose your breakfast'
+select item in done "${menu[@]}"
+do
+    # skip invalid selections ($item is empty)
+    [[ -z $item ]] && continue
+
+    # exit if done
+    [[ $item == done ]] && break
+
+    # store and print the item
+    order+=("$item")
+    echo "Added $item to your order"
+done
+
+# print the order
+echo -e "\nYou ordered the following ${#order[@]} items:"
+for item in "${order[@]}"
+do
+    echo "* $item"
+done
