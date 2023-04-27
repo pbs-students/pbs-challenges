5bc658956ce0f9d00e062ba1fcbc26f57f30f7d6
Allison Sheridan <allison@podfeet.com>
2023-04-26 19:09:36 -0700

    Feat: pbs149 Starting to add getopts stuff

--- podfeet/pbs149-getopts/getopts-challenge.sh
+++ podfeet/pbs149-getopts/getopts-challenge.sh
@@ -9,10 +9,38 @@
 # Each time the user selects an item, append it to an array representing their order. 
 # When the user is done adding items, print their order.
 # For bonus credit, update your script to load the menu into an array from a text file containing one menu item per line, ignoring empty lines and lines starting with a # symbol.
-# accept an optional argument limiting the number of items a user can order from the breakfast menu.
+# PBS148: accept an optional argument limiting the number of items a user can order from the breakfast menu.
+# PBS149: update your solution to the previous challenge to convert the optional argument for specifying a limit to a -l optional argument, and add a -s flag to enable snarky output (like the infamous Carrot weather app for iOS does).
 
-# assign the optional argument as the maximum food items that can be ordered
-maxFood=$1
+# Variable to hold snarkiness - assume it's blank
+isSnark=""
+
+usage="Usage: $(basename $0) [-s] [-l LIMIT]"
+
+# while loop to use getopts go through and look for arguments
+# Start with : to suppress error messages, I'll write my own
+# s goes first cuz it's a flag
+# : after the l because it has arguments
+# $opt will hold the matched options
+
+while getopts ':sl:' opt
+do
+  case $opt in
+    s)
+      # flag for snarkiness - 1 means to be snarky
+      isSnark = 1
+      ;;
+    l)
+      # optional argument to set a limit on how many items they can order
+      maxFood = "$OPTARG"
+      ;;
+    ?)
+      # here comes my fancy error message if they type something after the shell script name that isn't -s or -l
+      echo "$usage"
+      exit 1
+      ;;
+  esac
+done
 
 # regex allows whole positive numbers
 regex=^[+]?[0-9]+$
@@ -23,13 +51,12 @@ if [[ -z $maxFood ]] # if no argument supplied
     maxFood=2 # Arbitrary max number of items allowed to be ordered
     echo "The max items you can order is $maxFood"
   else
-  until [[ $maxFood =~ $regex ]]
+  until [[ $maxFood =~ $regex ]] # $maxFood is a positive number
     do
       read -p "Please enter a whole number, try again: " maxFood		
     done
   echo "The max items you can order is $maxFood"
 fi
-
 
 # Create an array of breakfast foods
 declare -a breakfastMenu
@@ -61,10 +88,8 @@ When you're done ordering, type 1 to select done"
 # NOTE: Quotes around the array in select required to keep items with spaces in their names as one item
 select food in "${breakfastMenu[@]}" 
   do 
-    
     # skip invalid selections ($food is empty)  
     [[ -z $food ]] && continue
-
 
     # exit if done
     [[ $food == done ]] && break
@@ -76,7 +101,6 @@ select food in "${breakfastMenu[@]}"
 
     # exit if $maxFood is reached
     [[ ${#order[@]} -eq $maxFood ]] && echo "You can't order any more food" && break
-    
 done
 
 echo "Let me read your order back to you:"
