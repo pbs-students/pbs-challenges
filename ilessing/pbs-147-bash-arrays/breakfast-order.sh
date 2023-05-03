b3974b6902caec50a28ab4d6f04163b9ef4478ad
Ian Lessing <ilessing@library.ucsb.edu>
2023-05-03 04:22:14 -0700

    user can now indicate they are done ordering
    the nested if statements make the script hard to read/understand at a glance.

--- ilessing/pbs-147-bash-arrays/breakfast-order.sh
+++ ilessing/pbs-147-bash-arrays/breakfast-order.sh
@@ -5,20 +5,39 @@
 declare -a mymenu
 
 # add items to the array
-mymenu=("eggs" "oatmeal" "croisant" "waffles" "toast" "yogurt")
+mymenu=("eggs" "oatmeal" "croisant" "waffles" "toast" "yogurt" "fruit")
 
 # push one more item to the end of the array
-mymenu+=(fruit)
+mymenu+=(done)
 
 # show count of menu items
 echo "there are ${#mymenu[@]} menu items available"
 
-# promt text for the user
+# prompt text for the user
+# PS3 is the prompt used by `select`
 PS3="select your breakfast: "
 
 # a select loop over the array
 select itemchosen in "${mymenu[@]}"
 do 
-  echo -e "\n you want $itemchosen.  Okay"
-  break
+  # test if they are done or not
+  if [[ $itemchosen != done ]]
+  then
+
+    # test if they have chosen something from the menu
+    if [[ -z $itemchosen ]]
+    then
+      echo "sorry bud.  that's not on the menu"
+    else
+    echo -e "\n you want $itemchosen.  Okay"
+  fi
+
+else
+
+  # double square brackets to test if user entered "done".  
+  # the break executes only if the expression in the square brackets resolves true
+  [[ $itemchosen == done ]] && break
+  fi
 done
+
+# prompt 
