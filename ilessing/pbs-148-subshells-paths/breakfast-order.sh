09d7bfa2b8b680c38bc90c22b9b691b8af158e41
Ian Lessing <ian@phpsiteworks.com>
2023-05-07 10:09:55 -0700

    feat: pbs148 challenge script accepts command line argument
    but does not yet impose the limit specified

---
+++ ilessing/pbs-148-subshells-paths/breakfast-order.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# usage: takes your breakfast order.  
+# Uses a set of menu items stored in a text file
+# breakfast-order.sh [number of items to order]
+
+
+declare -a mymenu
+declare -a myorder
+
+# read menu items from stored menu.txt file
+# skip blank lines and comment lines in the menu.text file
+while IFS= read
+  do
+    [[ -z $REPLY ]] && continue
+    [[ $REPLY =~ ^[\#] ]] && continue #not sure how/why this works. I don't understand the syntax.
+    mymenu+=("$REPLY")
+  done < menu.txt 
+
+# if no argument is passed, default to number of items in the menu
+# otherwise, use the argument passed to the script
+if [[ -z $1 ]] 
+then
+  numitems=${#mymenu[@]}
+  echo "no limit"
+# $1 is numeric
+elif [[ $1 =~ ^[0-9]+$ ]] && [[ $1 -le ${#mymenu[@]} ]]
+  then
+    numitems=$1
+    echo "limit is $1"
+  else
+  # user entered too high a number or a non-number
+  echo "out of range. Now, let's be reasonable.  I'll give you the whole menu"
+  numitems=${#mymenu[@]}
+fi
+
+echo "you can choose up to $numitems items from the menu"
+
+
+# push one more item to the end of the array
+mymenu+=(done)
+
+# echo "${mymenu[5]}"; exit; # expected value: "wheat toast"
+
+# show count of menu items
+echo "there are ${#mymenu[@]} menu items available"
+
+# prompt text for the user
+# PS3 is the prompt used by `select`
+PS3="select your breakfast: "
+
+# a select loop over the array
+select itemchosen in "${mymenu[@]}"
+do 
+  echo "you chose $itemchosen"
+  
+  # test if they are done or not
+  if [[ $itemchosen != done ]]
+  then
+    echo " is that on the menu?"
+    # test if they have chosen something from the menu
+    if [[ -z $itemchosen ]]
+    then
+      echo "sorry bud.  that's not on the menu"
+    else
+      # user made a valid menu selection so lets record their choice
+      # by adding their choice to an array
+      echo -e "\n you want $itemchosen.  Okay"
+      myorder+=("$itemchosen")
+    fi
+  else
+  # under what conditions is this part of the conditional executed??
+
+  # double square brackets to test if user entered "done".  
+  # the break executes only if the expression in the square brackets resolves true
+  [[ $itemchosen == done ]] && break
+
+  fi
+done
+
+echo -e "\n\n you ordered: ${myorder[@]} \n\n I'm sending it to the kitchen now."
---
+++ ilessing/pbs-148-subshells-paths/readme.md
@@ -0,0 +1,18 @@
+
+## pbs-148-bash-potpourri/
+
+
+
+In the meantime, if you want some more Bash practice, update your solution to the challenge to accept an optional argument limiting the number of items a user can order from the breakfast menu.
+
+You will find my solution in this folder.
+
+[PBS 148 Show Notes](https://pbs.bartificer.net/pbs148)
+
+### To Dos
+
+- [ ] use `$BASH_SOURCE` environment variable
+- [ ] use `dirname` and `basename` functions to derive correct paths
+- [ ] allow running the main script from some other directory
+- [ ] accept an argument
+- [ ] if agument is numeric treat it as item limit
