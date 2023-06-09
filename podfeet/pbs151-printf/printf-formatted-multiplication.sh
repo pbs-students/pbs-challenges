d0a6de03d184f51a831b46218840ec5c29de9d86
Allison Sheridan <allison@podfeet.com>
2023-06-09 14:14:54 -0700

    Feat: work on getopts, comment out error checking
    error getopts commannd not found and error checking a mess

--- podfeet/pbs151-printf/printf-formatted-multiplication.sh
+++ podfeet/pbs151-printf/printf-formatted-multiplication.sh
@@ -30,30 +30,39 @@
 # My solution goes a bit beyond and allows the user to define the specific range, not just 1-x
 
 # use the first argument as the number if it was given
-number=$1
-rangemin=$2
-rangemax=$3
+# number=$1
+# rangemin=$2
+# rangemax=$3
+
+number=5
+rangemin=1
+rangemax=10
 
 # new version will be to add optional arguments specifically
 usage="Usage: $(basename $0) [-m] [-M LIMIT]"
 # NOTE: I think n is wrong the way I'm doing it. It's required, not optional
-while getops ':nm:M:' opt # I'll do error checking, n is required, m and M are optional
+while getops ':n:m:M:' opt # I'll do error checking, n, m and M are optional flags
 do
 	case $opt in
-		n) # The number to be multiplied by
-			number=1
+		n) 
+			# The number to be multiplied by
+			number="$OPTARG"
 			;;
-		m) # Minimum value to use as multiplier
-			rangemin=$OPTARG
+		m) 
+			# Minimum value to use as multiplier
+			rangemin="$OPTARG"
 			;;
-		M) # Maximum value to use as multiplier
-			rangemax=$OPTARG
+		M) 
+			# Maximum value to use as multiplier
+			rangemax="$OPTARG"
 			;;
-		?) # What if they type something after the script name that isn't n, m, or M
-				echo "$usage"
-				exit 1
-				;;
-	ecase
+    ?)
+      # here comes my fancy error message if they type something after the shell
+      # script name that isn't -n, -m, -M
+      echo "$usage"
+      exit 1
+      ;;
+	esac
 done
 
 # Define a regular expression for a whole number to check each value against
@@ -65,58 +74,58 @@ regex=^[+-]?[0-9]+$
 # if no argument was provided ask for one
 # code to make sure it's really a whole number
 
-if [[ -z $number ]] # if no arguments were supplied
-	then
-		# Ask user for the whole number to multiply and optionally to define the range
-			read -p "Give me a whole number and I'll show you the times table for it: " number
-			until [[ $number =~ $regex ]]
-				do
-					read -p "That was not a whole number, try again: " number		
-				done	
-				# ask if user wants to define the range. If they answer anything but yes, it will use the default of 1-10
-				read -p "Do you want to define the range for the times table? Type yes (y) or no (n or enter) " yesno
-				# Quotes added around var $yesno otherwise I get a unary operator error
-				# Without the quotes, if the value doesn't exist, the variable vanishes, leaving if [ = "yes"]
-				# Single [] brackets are POSIX compatible. If I used double [] brackets I wouldn't need the "" around var
-				if [[ $yesno == 'yes' ]] || [[ $yesno == 'y' ]] || [[ $yesno == 'Y' ]] || [[ $yesno == 'YES' ]]
-				# if [ "$yesno" == 'yes' ] || [ "$yesno" == 'y' ] || [ "$yesno" == 'Y' ] || [ "$yesno" == 'YES' ]
-					then
-						# Keep asking till the user supplies a whole number for range min	
-						# Ask user for range minimum and assign to variable rangemin
-						read -p "Give me the MINIMUM value by which you want to multiply: " rangemin		
-						# ***********
-						until echo "$rangemin" | egrep -q $regex
-						# ****************
-							do
-								read -p "That was not a whole number, try again: " rangemin	
-							done
-						# Keep asking till the user supplies a whole number for range max
-						read -p "Now give me the MAX value by which you want to multiply: " rangemax
-						until echo "$rangemax" | egrep -q $regex
-							do
-								 read -p "That was not a whole number, try again: " rangemax
-							done
-				else
-					rangemin=1
-					rangemax=10
-				fi
-else
-	number=$1
-	if [[ $number =~ $regex ]]
-		then
-			if [[ -z $2 ]] # if there is no second argument, there cannot be a third so do not worry about it
-				then # set range min/max to defaults
-					rangemin=1
-					rangemax=10
-				else # assume there is a second and third argument and use them
-					# if there is no third argument, it multiplies down to zero. That sure is convenient!
-					rangemin=$2
-					rangemax=$3
-			fi
-		else
-			echo "That was not a whole number"
-		fi
-fi
+# if [[ -z $number ]] # if no arguments were supplied
+# 	then
+# 		# Ask user for the whole number to multiply and optionally to define the range
+# 			read -p "Give me a whole number and I'll show you the times table for it: " number
+# 			until [[ $number =~ $regex ]]
+# 				do
+# 					read -p "That was not a whole number, try again: " number		
+# 				done	
+# 				# ask if user wants to define the range. If they answer anything but yes, it will use the default of 1-10
+# 				read -p "Do you want to define the range for the times table? Type yes (y) or no (n or enter) " yesno
+# 				# Quotes added around var $yesno otherwise I get a unary operator error
+# 				# Without the quotes, if the value doesn't exist, the variable vanishes, leaving if [ = "yes"]
+# 				# Single [] brackets are POSIX compatible. If I used double [] brackets I wouldn't need the "" around var
+# 				if [[ $yesno == 'yes' ]] || [[ $yesno == 'y' ]] || [[ $yesno == 'Y' ]] || [[ $yesno == 'YES' ]]
+# 				# if [ "$yesno" == 'yes' ] || [ "$yesno" == 'y' ] || [ "$yesno" == 'Y' ] || [ "$yesno" == 'YES' ]
+# 					then
+# 						# Keep asking till the user supplies a whole number for range min	
+# 						# Ask user for range minimum and assign to variable rangemin
+# 						read -p "Give me the MINIMUM value by which you want to multiply: " rangemin		
+# 						# ***********
+# 						until echo "$rangemin" | egrep -q $regex
+# 						# ****************
+# 							do
+# 								read -p "That was not a whole number, try again: " rangemin	
+# 							done
+# 						# Keep asking till the user supplies a whole number for range max
+# 						read -p "Now give me the MAX value by which you want to multiply: " rangemax
+# 						until echo "$rangemax" | egrep -q $regex
+# 							do
+# 								 read -p "That was not a whole number, try again: " rangemax
+# 							done
+# 				else
+# 					rangemin=1
+# 					rangemax=10
+# 				fi
+# else
+# 	number=$1
+# 	if [[ $number =~ $regex ]]
+# 		then
+# 			if [[ -z $2 ]] # if there is no second argument, there cannot be a third so do not worry about it
+# 				then # set range min/max to defaults
+# 					rangemin=1
+# 					rangemax=10
+# 				else # assume there is a second and third argument and use them
+# 					# if there is no third argument, it multiplies down to zero. That sure is convenient!
+# 					rangemin=$2
+# 					rangemax=$3
+# 			fi
+# 		else
+# 			echo "That was not a whole number"
+# 		fi
+# fi
 
 # Check to see if they put in a bigger min than max, and count down instead if so
 # --- Change to use printf ---
